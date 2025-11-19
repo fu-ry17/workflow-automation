4cb96746b548e18e62a8c806b59cb32b56f24dfc
Fu-ry17 <yewabrian17@gmail.com>
2025-11-20 00:54:56 +0300

    upgraded to prisma version 7

--- frontend/src/lib/prisma.ts
+++ frontend/src/lib/prisma.ts
@@ -1,13 +1,14 @@
 import { PrismaClient } from "@/generated/prisma/client";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    accelerateUrl: process.env.DATABASE_URL!,
+  }).$extends(withAccelerate()) as unknown as PrismaClient;
 };
 
-const prisma =
-  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
+export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
-export default prisma;
